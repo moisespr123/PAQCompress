cd1fea7be9f20ba7d08ab58f443907f89f33d02b
moisesmcardona <moises@moises-studios.com>
2019-01-13 10:06:05 -0500

    Bumped to version 0.3

--- PAQCompress/My Project/AssemblyInfo.vb
+++ PAQCompress/My Project/AssemblyInfo.vb
@@ -11,8 +11,8 @@ Imports System.Runtime.InteropServices
 <Assembly: AssemblyTitle("PAQCompress")> 
 <Assembly: AssemblyDescription("")> 
 <Assembly: AssemblyCompany("")> 
-<Assembly: AssemblyProduct("PAQCompress")> 
-<Assembly: AssemblyCopyright("Copyright ©  2008")> 
+<Assembly: AssemblyProduct("PAQCompress")>
+<Assembly: AssemblyCopyright("Copyright ©  2008 - 2019")>
 <Assembly: AssemblyTrademark("")> 
 
 <Assembly: ComVisible(False)>
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("0.2.0.0")>
-<Assembly: AssemblyFileVersion("0.2.0.0")>
+<Assembly: AssemblyVersion("0.3.0.0")>
+<Assembly: AssemblyFileVersion("0.3.0.0")>
