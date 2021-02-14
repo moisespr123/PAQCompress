5ab1f0612f06b463a04c2054a23fbc7503c3e522
moisesmcardona <moises@moises-studios.com>
2021-02-13 19:15:05 -0500

    v0.5.8: Added paq8pxd v101.

--- PAQCompress/Form1.Designer.vb
+++ PAQCompress/Form1.Designer.vb
@@ -416,7 +416,7 @@ Partial Class Form1
         Me.Label3.Name = "Label3"
         Me.Label3.Size = New System.Drawing.Size(161, 13)
         Me.Label3.TabIndex = 21
-        Me.Label3.Text = "v0.5.7 - GUI by: Moisés Cardona"
+        Me.Label3.Text = "v0.5.8 - GUI by: Moisés Cardona"
         '
         'paq_other
         '
