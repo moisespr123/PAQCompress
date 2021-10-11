eaa97b4a24c61b760d6283924f0147dca7996da9
moisesmcardona <moises@moises-studios.com>
2021-10-10 20:24:39 -0400

    v0.5.16: Added paq8pxd v105.

--- PAQCompress/My Project/AssemblyInfo.vb
+++ PAQCompress/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("0.5.15.0")>
-<Assembly: AssemblyFileVersion("0.5.15.0")>
+<Assembly: AssemblyVersion("0.5.16.0")>
+<Assembly: AssemblyFileVersion("0.5.16.0")>
